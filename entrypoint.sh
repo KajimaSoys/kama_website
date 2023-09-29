2e5139560902368728694f74d7bd540f585be79e
Grishin Vadim <43302420+KajimaSoys@users.noreply.github.com>
2023-09-30 02:29:08 +0400

    v.1.1 - Entrypoint fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-exec gunicorn rezal_prom_website.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn kama_website.wsgi:application --bind 0.0.0.0:8000
